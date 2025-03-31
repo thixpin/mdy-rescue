0fa9a3460f7ec017095c3ec82632b5bd9b8c4213
Soe Thura <thixpin@gmail.com>
2025-03-31 12:58:45 +0700

    fix: AlpineJS

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -99,6 +99,6 @@
     </div>
 
     <!-- Alpine.js -->
-    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <script src="{{ asset('js/alpine.min.js') }}" defer></script>
 </body>
 </html> 
